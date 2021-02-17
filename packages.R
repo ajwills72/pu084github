0b8206fb97431f51a1c71363a8315d6e891b8993
Andy J. Wills <andy@willslab.co.uk>
2021-02-17 10:34:26 +0000

    Housekeeping

--- packages.R
+++ packages.R
@@ -9,3 +9,4 @@ library(BayesFactor) ## Bayesian contingency tests
 library(effsize)     ## Calculation of Cohen's d
 library(xfun)        ## Convert numbers to words
 library(tools)       ## Convert to words to title case
+library(papaja)      ## APA-format manuscripts
